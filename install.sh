04e8cfadac6af3cf617859677915ea5094709d89
/:) <g.yevgeniy.p@gmail.com>
2022-07-08 16:05:34 +0200

    + base functionality

---
+++ install.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# Author: Yevgeniy Goncharov aka xck, http://sys-adm.in
+# KeyDB installer for Debian-based distros
+
+# Envs
+# ---------------------------------------------------\
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+# Output messages
+# ---------------------------------------------------\
+
+# And colors
+RED='\033[0;91m'
+GREEN='\033[0;92m'
+CYAN='\033[0;96m'
+YELLOW='\033[0;93m'
+PURPLE='\033[0;95m'
+BLUE='\033[0;94m'
+BOLD='\033[1m'
+WHiTE="\e[1;37m"
+NC='\033[0m'
+
+ON_SUCCESS="DONE"
+ON_FAIL="FAIL"
+ON_ERROR="Oops"
+ON_CHECK="✓"
+
+Info() {
+  echo -en "[${1}] ${GREEN}${2}${NC}\n"
+}
+
+Warn() {
+  echo -en "[${1}] ${PURPLE}${2}${NC}\n"
+}
+
+Success() {
+  echo -en "[${1}] ${GREEN}${2}${NC}\n"
+}
+
+Error () {
+  echo -en "[${1}] ${RED}${2}${NC}\n"
+}
+
+Splash() {
+  echo -en "${WHiTE} ${1}${NC}\n"
+}
+
+space() { 
+  echo -e ""
+}
+
+# Functions
+# ---------------------------------------------------\
+
+# Check is current user is root
+isRoot() {
+  if [ $(id -u) -ne 0 ]; then
+    Error $ON_ERROR "You must be root user to continue"
+    exit 1
+  fi
+  RID=$(id -u root 2>/dev/null)
+  if [ $? -ne 0 ]; then
+    Error "User root no found. You should create it to continue"
+    exit 1
+  fi
+  if [ $RID -ne 0 ]; then
+    Error "User root UID not equals 0. User root must have UID 0"
+    exit 1
+  fi
+}
+
+# Checks supporting distros
+checkDistro() {
+  # Checking distro
+  if [ -e /etc/centos-release ]; then
+      DISTRO=`cat /etc/redhat-release | awk '{print $1,$4}'`
+      RPM=1
+  elif [ -e /etc/fedora-release ]; then
+      DISTRO=`cat /etc/fedora-release | awk '{print ($1,$3~/^[0-9]/?$3:$4)}'`
+      RPM=2
+  elif [ -e /etc/os-release ]; then
+    DISTRO=`lsb_release -d | awk -F"\t" '{print $2}'`
+    RPM=0
+    DEB=1
+  else
+      Error "Your distribution is not supported (yet)"
+      exit 1
+  fi
+}
+
+apt_actions() {
+    echo "deb https://download.keydb.dev/open-source-dist $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/keydb.list
+    sudo wget -O /etc/apt/trusted.gpg.d/keydb.gpg https://download.keydb.dev/open-source-dist/keyring.gpg
+    sudo apt update
+    sudo apt install keydb-server keydb-tools -y
+}
+
+# Actions
+# ---------------------------------------------------\
+
+init() {
+
+    if [[ "$DEB" -eq "1" ]]; then
+        Info "$ON_CHECK" "Run Debian installer..."
+        echo -e "[${GREEN}✓${NC}] Install Debian packages"
+        apt_actions
+    else
+        Info "$ON_CHECK" "Nit supported distro. Exit..."
+        exit 1
+    fi
+
+}
+
+isRoot
+checkDistro
+
+init
